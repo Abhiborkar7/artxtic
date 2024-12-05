5daf8437e09b3836a594923af2df0d3c1e85d090
Abhishek Borkar <2022bcs096@sggs.ac.in>
2024-12-05 11:54:55 +0530

    changed tailwind config to ESM

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
     darkMode: ['class'],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -131,6 +133,6 @@ export default {
   	}
   },
   plugins: [
-    require("tailwindcss-animate")
+    tailwindcssAnimate
   ],
 };
